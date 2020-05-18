87cf93025dd5aea0c4f677fd68cdfa2906fba9a7
Claudia Narratone <cnarratone@alice.it>
2020-05-18 18:08:47 +0200

    Aggiunti metodi visita grafo e creazione albero

--- src/main/java/it/polito/tdp/metroparis/model/Model.java
+++ src/main/java/it/polito/tdp/metroparis/model/Model.java
@@ -1,13 +1,20 @@
 package it.polito.tdp.metroparis.model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.jgrapht.Graph;
 import org.jgrapht.Graphs;
+import org.jgrapht.event.ConnectedComponentTraversalEvent;
+import org.jgrapht.event.EdgeTraversalEvent;
+import org.jgrapht.event.TraversalListener;
+import org.jgrapht.event.VertexTraversalEvent;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleDirectedGraph;
+import org.jgrapht.traverse.BreadthFirstIterator;
+import org.jgrapht.traverse.DepthFirstIterator;
 
 import it.polito.tdp.metroparis.db.MetroDAO;
 
@@ -56,12 +63,92 @@ public class Model {
 		
 		
 		//System.out.println(this.graph);
-		System.out.format("Grafo caricato con %d vertici e %d archi", 
+		System.out.format("Grafo caricato con %d vertici e %d archi \n", 
 				this.graph.vertexSet().size(),
 				this.graph.edgeSet().size());
 	}
+	
+	//metodo che visita l'intero grafico con strategia Breadth First e ritorna l'insieme di vertici incontrati
+	public List<Fermata> visitaAmpiezza(Fermata source) {
+		BreadthFirstIterator<Fermata, DefaultEdge> bfv=new BreadthFirstIterator<>(graph, source);
+		List<Fermata> visita=new ArrayList<>();
+		
+		while(bfv.hasNext()) {
+			visita.add(bfv.next());
+		}
+		return visita;
+	}
+	
+	//metodo che restituisce l'albero della ricerca breadth first restituisce mappa<verticeNuovoScoperto, verticePrecedenteDaCuiArrivavo>
+	public Map<Fermata, Fermata> alberoVisita(Fermata source) {
+		
+		Map<Fermata, Fermata> alberoMap=new HashMap<Fermata, Fermata>();
+		alberoMap.put(source, null);
+		
+		BreadthFirstIterator<Fermata, DefaultEdge> bfv=new BreadthFirstIterator<>(graph, source);
+		bfv.addTraversalListener(new TraversalListener<Fermata, DefaultEdge>() {
 
+			@Override
+			public void connectedComponentFinished(ConnectedComponentTraversalEvent e) {
+			}
+
+			@Override
+			public void connectedComponentStarted(ConnectedComponentTraversalEvent e) {
+			}
+
+			@Override
+			public void edgeTraversed(EdgeTraversalEvent<DefaultEdge> e) {
+				//la visita sta considerando un nuovo arco
+				//questo arco ha scoperto un nuovo vertice?
+				//se si, provenendo da dove?
+				DefaultEdge edge=e.getEdge(); //(a, b) : ho scoperto a partendo da b o viceversa?
+				Fermata a= graph.getEdgeSource(edge);
+				Fermata b= graph.getEdgeTarget(edge);
+				if(alberoMap.containsKey(a)) {
+					alberoMap.put(b, a);
+				}else {
+					alberoMap.put(a, b);
+				}
+			}
+
+			@Override
+			public void vertexTraversed(VertexTraversalEvent<Fermata> e) {
+			
+			}
+
+			@Override
+			public void vertexFinished(VertexTraversalEvent<Fermata> e) {
+			
+			}
+		});
+		
+		while(bfv.hasNext()) {
+			bfv.next();
+		}
+		return alberoMap;
+	}
+	
+	//metodo che visita l'intero grafico con strategia Depth First e ritorna l'insieme di vertici incontrati
+	public List<Fermata> visitaProfondita(Fermata source) {
+		DepthFirstIterator<Fermata, DefaultEdge> dfv=new DepthFirstIterator<>(graph, source);
+		List<Fermata> visita=new ArrayList<>();
+		
+		while(dfv.hasNext()) {
+			visita.add(dfv.next());
+		}
+		return visita;
+	}
+	
 	public static void main(String args[]) {
 		Model m=new Model();
+		List<Fermata> visita1=m.visitaAmpiezza(m.fermate.get(0));
+		System.out.println(visita1);
+		List<Fermata> visita2=m.visitaProfondita(m.fermate.get(0));
+		System.out.println(visita2);
+		
+		Map<Fermata, Fermata> albero= m.alberoVisita(m.fermate.get(0));
+		for(Fermata f: albero.keySet()) {
+			System.out.format("%s <- %s \n", f, albero.get(f));
+		}
 	}
 }
